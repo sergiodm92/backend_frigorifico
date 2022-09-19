c3598f0c0e2179ef8435c6acabb4cbb02ef1cfb7
Sergio Andrés David Maioli <97261829+sergiodm92@users.noreply.github.com>
2022-09-19 01:29:06 -0300

    Update Venta.js

--- src/models/Venta.js
+++ src/models/Venta.js
@@ -47,5 +47,9 @@ module.exports = (sequelize) => {
       type: DataTypes.FLOAT,
       allowNull: true,
     },
+    saldo: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
   });
 };
