27b1f16e00ee39db28f33a5c41dadada4d4693b8
Fede Lahoz <fedexaz3@gmail.com>
2022-09-18 23:54:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('Servidor iniciado en puerto ' + process.env.PORT);
   });
