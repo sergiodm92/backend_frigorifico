04609f65eda5463e32599520d90afdf2a53d4f17
Sergio Andrés David Maioli <97261829+sergiodm92@users.noreply.github.com>
2022-09-20 12:05:58 -0300

    Update faenas.js

--- src/routes/faenas.js
+++ src/routes/faenas.js
@@ -42,7 +42,7 @@ route.post('/', (req, res) => {
     if(crearFaena(req.body)){
         return res.status(201).send(customResponseExito("Faena creada con éxito"));
     }
-    return res.status(400).send(customResponseError("Error al crear la faena, verificar tropa", 400));
+    else return res.status(400).send(customResponseError("Error al crear la faena, verificar tropa", 400));
 })
 
 route.put('/', async (req, res) => {
