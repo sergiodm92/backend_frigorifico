99eb03e58a7944be6a9b65409209337a588e340a
sergiodm92 <97261829+sergiodm92@users.noreply.github.com>
2022-10-03 19:50:19 -0300

    correlativo tipo string

--- src/models/Res.js
+++ src/models/Res.js
@@ -8,7 +8,7 @@ module.exports = (sequelize) => {
         primaryKey: true,
     },
     correlativo: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: true,
     },
     categoria: {
