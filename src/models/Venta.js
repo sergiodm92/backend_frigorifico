8f1afbfd848b81f5fb1addfb2786dabd33a53553
Sergio Andrés David Maioli <97261829+sergiodm92@users.noreply.github.com>
2022-10-04 17:28:43 -0300

    Update Venta.js

--- src/models/Venta.js
+++ src/models/Venta.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   sequelize.define('venta', {
-    ID: {
+    id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
